7c887973549faa0cf7a2477f0e5cfc21c15e847d
alireza <ashabanpisheh@gmail.com>
2025-04-03 14:59:41 +0330

    mmm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
-      include: ['/myDashboard'],
+      include: ['mydashboard'],
       exclude: [],
       saveRedirectToCookie: true,
     }
