2ef948ae5b7d9609654401a3119398ea76409f5e
alireza <ashabanpisheh@gmail.com>
2025-06-08 22:30:18 +0330

    header section - js to vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,48 @@
 <script setup>
+const navbarVisible = ref(false);
 
+function toggleNavbar() {
+  navbarVisible.value = !navbarVisible.value;
+}
+</script>
 
+<template>
+  <div>
+    <header>
+      <div class="humburger" @click="toggleNavbar">
+        <i 
+          class="fa-solid fa-bars-staggered" 
+          :data-visible="!navbarVisible"
+        ></i>
+        <i 
+          class="fa-solid fa-xmark" 
+          :data-visible="navbarVisible"
+        ></i>
+      </div>
+      <nav class="container navbar">
+        <img src="../assets/images/logo.png" alt="logo" class="logo">
+        <img src="../assets/images/logo 2.png" alt="logo" class="mobile-logo">
+
+        <ul 
+          class="nav-links" 
+          :data-visible="navbarVisible"
+        >
+          <li><a href="#About">About me</a></li>
+          <li><a href="#Skills">Skills</a></li>
+          <li><a href="#portofolio">Portfolio</a></li>
+          <a class="btn" href="#contact">Contact Me</a>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<!-- ورژن قبلی - جاوا اسکریپت خام  -->
+
+<!-- <script setup>
 onMounted(() => {
   const navbar = document.querySelector(".nav-links");
   const humbergur = document.querySelector(".humburger");
@@ -19,8 +61,6 @@ onMounted(() => {
     }
   });
 });
-
-
 </script>
 
 <template>
@@ -31,10 +71,8 @@ onMounted(() => {
             <i class="fa-solid fa-xmark"  data-visible="false"></i>
         </div>
       <nav class="container navbar">
-       
         <img src="../assets/images/logo.png" alt="logo" class="logo">
         <img src="../assets/images/logo 2.png" alt="logo" class="mobile-logo">
-
         <ul class="nav-links"  data-visible="false">
             <li><a href="#About">About me</a></li>
             <li><a href="#Skills">Skills</a></li>
@@ -47,4 +85,4 @@ onMounted(() => {
 </template>
 
 <style scoped>
-</style>
+</style> -->
